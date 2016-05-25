60f68f13fe58a82d01664654c8d2cc6b742a13d0
nelson1122 <nelson8533@gmail.com>
2016-05-25 15:17:01 -0500

    Update polygon_linestring_migration.py

--- geocoder/data/polygon_linestring_migration.py
+++ geocoder/data/polygon_linestring_migration.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 #
-#Migracion de geometrias Linea y Polygono OSM a PostgreSQL
-#
-#Este algoritmo permite la migracion de communas, barrios y vias digitalizadas
-#con el editor JOSM al esquema de datos del geocodificador instalado en PostgreSQL
-#
+#  polygon_linestring_migration.py
+#  
+#  Copyright 2016 Nelson Enrique Quemá <nelson8533@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
 #
 import sys, getopt, xml, re, psycopg2, string
 
